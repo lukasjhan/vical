7276b4afd4f6562b1b937a024dc8b8b7b637d1e3
Lukas.J Han <lukas@hopae.io>
2025-11-26 11:39:07 +0900

    chore: remove korean comments

--- src/test.ts
+++ src/test.ts
@@ -1,18 +1,14 @@
 import { parseVICAL, buildTrustAnchors } from "./index";
 import * as fs from "fs";
 
-// VICAL 파일 읽기
 const vicalBytes = fs.readFileSync("./test.cbor");
-
-// 파싱
 const signedVical = parseVICAL(vicalBytes);
 
 console.log(signedVical);
 
-// 기본 정보
-console.log(signedVical.vical.version); // "1.0"
-console.log(signedVical.vical.vicalProvider); // "Korea VICAL Service"
-console.log(signedVical.vical.certificateInfos); // 인증서 개수
+console.log("Version:", signedVical.vical.version);
+console.log("Provider:", signedVical.vical.vicalProvider);
+console.log("Certificate count:", signedVical.vical.certificateInfos.length);
+console.log("Certificates:", signedVical.vical.certificateInfos);
 
-// Trust Anchor 맵 생성 (국가별 IACA)
 const trustAnchors = buildTrustAnchors(signedVical.vical);
